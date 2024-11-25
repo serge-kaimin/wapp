f228bb96b3fc2288b9f26d096ce948f0bf3c630f
Serge Kaimin <serge.kaimin@gmail.com>
2024-11-25 19:55:33 +0400

    Add YourModel with name and description fields

--- backend/backend_api/models.py
+++ backend/backend_api/models.py
@@ -1,3 +1,10 @@
+# backend_api/models.py
+
 from django.db import models
 
-# Create your models here.
+class YourModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
